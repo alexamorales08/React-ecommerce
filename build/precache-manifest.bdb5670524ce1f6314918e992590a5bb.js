4a28005d1648415adb7e6211e6f8ab7811116c14
alexamorales08 <32378169+alexamorales08@users.noreply.github.com>
2021-02-24 04:45:06 +0200

    Add files via upload

---
+++ build/precache-manifest.bdb5670524ce1f6314918e992590a5bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c45bc3866463162c2e5d24c136f0e0e",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "3eafe1d207c1435136e5",
+    "url": "/my-app/static/js/2.d7965701.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/my-app/static/js/2.d7965701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85bf7d673f60c915d784",
+    "url": "/my-app/static/js/main.f99fa128.chunk.js"
+  },
+  {
+    "revision": "247107d9248038dd093e",
+    "url": "/my-app/static/js/runtime-main.378ae7c2.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/my-app/static/media/commerce.422fec10.png"
+  }
+]);
